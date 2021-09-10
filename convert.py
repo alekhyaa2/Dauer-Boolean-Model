1f2832abe0284577483a4d003415240139dd0eb6
alekhyaa2 <78366782+alekhyaa2@users.noreply.github.com>
2021-09-09 23:12:46 -0500

    Update convert.py

--- convert.py
+++ convert.py
@@ -6,6 +6,7 @@ import boolparser
 import gprops
 import subgraph
 
+#the brackets have to be {} and not () for the file to be read.
 rulefile = 'dauer_rules.txt'
 G = boolparser.readfile(rulefile)
 gprops.set_edge_type(G)
